561e472ab1bc89aa9edb72955f05a7b90590ccdf
MartinIsti <martinisti@gmail.com>
2020-12-14 12:53:36 +1300

    Update 01 - ResourceGroup.ps1

--- 01 - ResourceGroup.ps1
+++ 01 - ResourceGroup.ps1
@@ -1,6 +1,19 @@
-# Create a Resource Group if needed:
+# Parameters
+    $subscriptionName = 'Microsoft Azure Enterprise'
+    $subscriptionId = '88ac0d8e-21ae-4e09-bc04-fcc13e4bcec8'
+    $tenantId = '4c5789e1-0364-48a8-88ba-09202a830218'
+    $orgName = 'istvanmartinka2'
+    $gitHubRepoUrl = "https://github.com/$orgName/<repo name>"
+
     $rgName = 'RG-Test' #to be parameterised
     $defaultLocation = 'australiaeast'
+
+# Login
+    az login
+    az account set --subscription $subscriptionName
+    az devops configure --defaults organization=https://dev.azure.com/$orgName
+
+# Create a Resource Group if needed:
 
     $rgExists = (az group exists --name $rgName)
     $rgExists
