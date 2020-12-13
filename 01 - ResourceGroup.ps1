690b6a380a6834a0b0c927c0110aee320cc897fc
MartinIsti <martinisti@gmail.com>
2020-12-14 12:35:45 +1300

    Update 01 - ResourceGroup.ps1

--- 01 - ResourceGroup.ps1
+++ 01 - ResourceGroup.ps1
@@ -3,7 +3,11 @@
     $defaultLocation = 'australiaeast'
 
     $rgExists = (az group exists --name $rgName)
+    $rgExists
 
+    #if ($rgExists -eq 'false') {
     if (-not $rgExists) {
         az group create --name $rgName --location $defaultLocation
+    } else {
+        Write 'RG already exists. Show some mercy.'
     }
