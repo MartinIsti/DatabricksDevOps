ec37beb69b54674939ee68197ac1f237c775fc2e
MartinIsti <martinisti@gmail.com>
2020-12-16 11:55:03 +1300

    Add files via upload

---
+++ 03 - KeyVault.ps1
@@ -0,0 +1,32 @@
+# parameters
+    [CmdletBinding()]
+    param(
+        $rgName,
+        $storageAccountName
+    )
+
+    Write-Host $rgName
+    Write-Host $storageAccountName
+
+# variables
+    $rawContainer = 'raw-storage'
+    $stageContainer = 'stage-storage'
+    $processContainer = 'processed-storage'
+       
+# check storage account existence
+    $storageNameAvailable = az storage account check-name --name $storageAccountName --query nameAvailable 
+    
+    # Write-Host $storageNameAvailable
+
+     if ($storageNameAvailable -eq 'true') {
+        # storage account
+        az storage account create --name $storageAccountName --resource-group $rgName --sku Standard_RAGRS --kind StorageV2
+
+        # connection string to avoid permission warnings
+        $connectionString=az storage account show-connection-string --name $storageAccountName --query connectionString -o tsv
+
+        # containers
+        az storage container create --name $rawContainer     --connection-string $connectionString
+        az storage container create --name $stageContainer   --connection-string $connectionString
+        az storage container create --name $processContainer --connection-string $connectionString
+    }
