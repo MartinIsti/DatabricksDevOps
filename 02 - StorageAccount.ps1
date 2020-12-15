2e47f15a7cfdcc9f874533e0473f1662b14bba5a
MartinIsti <martinisti@gmail.com>
2020-12-15 16:37:20 +1300

    Update 02 - StorageAccount.ps1

--- 02 - StorageAccount.ps1
+++ 02 - StorageAccount.ps1
@@ -9,20 +9,22 @@
     $rawContainer = 'raw-storage'
     $stageContainer = 'stage-storage'
     $processContainer = 'processed-storage'
+    
+    $storageAccountName2 = 'storageacc20201215'
 
 # check storage account existence
     # $storageNameAvailable = 
     # az storage account check-name --name $storageAccountName # --query nameAvailable
-    az storage account check-name --name 'storageacc20201215'
+    az storage account check-name --name $storageAccountName2
     
     # Write-Host $storageNameAvailable
 
     # if ($storageNameAvailable -eq 'true') {
         # storage account
-        # az storage account create --name $storageAccountName --resource-group $rgName --sku Standard_RAGRS --kind StorageV2
+        # az storage account create --name $storageAccountName2 --resource-group $rgName --sku Standard_RAGRS --kind StorageV2
 
         # connection string to avoid permission warnings
-        # $connectionString=az storage account show-connection-string --name $storageAccountName --query connectionString -o tsv
+        # $connectionString=az storage account show-connection-string --name $storageAccountName2 --query connectionString -o tsv
 
         # containers
         # az storage container create --name $rawContainer     --connection-string $connectionString
