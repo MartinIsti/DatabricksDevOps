f602f7303902a3e8ccb3267e00ca6b5bb1f40a63
MartinIsti <martinisti@gmail.com>
2020-12-15 17:08:38 +1300

    Update 02 - StorageAccount.ps1

--- 02 - StorageAccount.ps1
+++ 02 - StorageAccount.ps1
@@ -13,23 +13,22 @@
     $stageContainer = 'stage-storage'
     $processContainer = 'processed-storage'
     
-    $storageAccountName2 = 'storageacc20201215'
+    # $storageAccountName2 = 'storageacc20201215'
     
 # check storage account existence
-    #$storageNameAvailable = 
-    az storage account check-name --name $storageAccountName --query nameAvailable 
+    $storageNameAvailable = az storage account check-name --name $storageAccountName --query nameAvailable 
     
     # Write-Host $storageNameAvailable
 
-    # if ($storageNameAvailable -eq 'true') {
+     if ($storageNameAvailable -eq 'true') {
         # storage account
-        # az storage account create --name $storageAccountName2 --resource-group $rgName --sku Standard_RAGRS --kind StorageV2
+        az storage account create --name $storageAccountName2 --resource-group $rgName --sku Standard_RAGRS --kind StorageV2
 
         # connection string to avoid permission warnings
-        # $connectionString=az storage account show-connection-string --name $storageAccountName2 --query connectionString -o tsv
+        $connectionString=az storage account show-connection-string --name $storageAccountName --query connectionString -o tsv
 
         # containers
-        # az storage container create --name $rawContainer     --connection-string $connectionString
-        # az storage container create --name $stageContainer   --connection-string $connectionString
-        # az storage container create --name $processContainer --connection-string $connectionString
+        az storage container create --name $rawContainer     --connection-string $connectionString
+        az storage container create --name $stageContainer   --connection-string $connectionString
+        az storage container create --name $processContainer --connection-string $connectionString
     # }
