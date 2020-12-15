876075554c80da6ce6d4b238c620b4ce12b5eb28
MartinIsti <martinisti@gmail.com>
2020-12-15 16:51:18 +1300

    Update 02 - StorageAccount.ps1

--- 02 - StorageAccount.ps1
+++ 02 - StorageAccount.ps1
@@ -11,7 +11,8 @@
     $processContainer = 'processed-storage'
     
 # check storage account existence
-    $storageNameAvailable = az storage account check-name --name $storageAccountName --query nameAvailable 
+    $storageNameAvailable = az storage account check-name --name $storageAccountName --output tsv 
+    # --query nameAvailable 
     
     Write-Host $storageNameAvailable
 
