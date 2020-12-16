27d31970a5ab7488c5f5587d010f84d59086300a
MartinIsti <martinisti@gmail.com>
2020-12-16 14:28:01 +1300

    Update 05 - AzureDatabricks.ps1

--- 05 - AzureDatabricks.ps1
+++ 05 - AzureDatabricks.ps1
@@ -19,7 +19,7 @@
 
     if (!$resourceExists) {
         Write-Host "Resource does not exist. Welcome the Creator!"
-        az databricks workspace --resource-group $rgName --name $databricksWorkspace
+        az databricks workspace create --resource-group $rgName --name $databricksWorkspace
     } else {
         Write-Host "Resource already exists I sit tight."
     }
