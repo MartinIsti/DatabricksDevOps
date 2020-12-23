76136d6a41c1b27736d63abc952c451c384e7796
MartinIsti <martinisti@gmail.com>
2020-12-23 18:00:36 +1300

    Update cluster.create.sh

--- databricks/cluster.create.sh
+++ databricks/cluster.create.sh
@@ -10,18 +10,18 @@ clusterName=$1
 cat cluster.template.json | sed "s/CLUSTER_NAME/$clusterName/g" > /tmp/conf.json
 
 echo "Creating Cluster"
-clusterID=$(databricks clusters create --json-file /tmp/conf.json | jq -r '.cluster_id')
+# clusterID=$(databricks clusters create --json-file /tmp/conf.json | jq -r '.cluster_id')
 
 STATE=$(databricks clusters list --output json | jq -r --arg I "$clusterID" '.clusters[] | select(.cluster_id == $I) | .state')
 
-echo "Wait for cluster to be PENDING"
-while [[ "$STATE" != "PENDING" ]]
-do
-    STATE=$(databricks clusters list --output json | jq -r --arg I "$clusterID" '.clusters[] | select(.cluster_name == $I) | .state')
-done
+# echo "Wait for cluster to be PENDING"
+# while [[ "$STATE" != "PENDING" ]]
+# do
+#     STATE=$(databricks clusters list --output json | jq -r --arg I "$clusterID" '.clusters[] | select(.cluster_name == $I) | .state')
+# done
 
 # the API is flaky and the library install complains about terminated clusters
 # so wait a bit more before continuing task
-sleep 10
+# sleep 10
 
-echo "Cluster $clusterID is pending"
+# echo "Cluster $clusterID is pending"
