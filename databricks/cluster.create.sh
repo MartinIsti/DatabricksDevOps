c218f3c3d44fdf4957aab52e88bcdaa403288bc9
MartinIsti <martinisti@gmail.com>
2020-12-23 17:35:00 +1300

    Create cluster.create.sh

---
+++ databricks/cluster.create.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# cluster name must be passed as parameter
+clusterName=$1
+[[ -z "$clusterName" ]] && exit 1
+
+# name the cluster based on variable
+# modify the configuration JSON with an environment suffix for the cluster name
+cat config.cluster.json | sed "s/CLUSTER_NAME/$clusterName/g" > /tmp/conf.json
+
+echo "Creating Cluster"
+clusterID=$(databricks clusters create --json-file /tmp/conf.json | jq -r '.cluster_id')
+
+STATE=$(databricks clusters list --output json | jq -r --arg I "$clusterID" '.clusters[] | select(.cluster_id == $I) | .state')
+
+echo "Wait for cluster to be PENDING"
+while [[ "$STATE" != "PENDING" ]]
+do
+    STATE=$(databricks clusters list --output json | jq -r --arg I "$clusterID" '.clusters[] | select(.cluster_name == $I) | .state')
+done
+
+# the API is flaky and the library install complains about terminated clusters
+# so wait a bit more before continuing task
+sleep 10
+
+echo "Cluster $clusterID is pending"
